302e2fd14da38715b0213e272457dbfa7630fc31
Alan <alan@sigma.iimas.unam.mx>
2026-01-22 12:40:57 -0600

    Updated src

--- src/NTRsurv.jl
+++ src/NTRsurv.jl
@@ -1,6 +1,31 @@
 module NTRsurv
 
-using Distributions, Roots
-import SpecialFunctions.gamma
+using Distributions, ProgressMeter
+import SpecialFunctions: gamma
+import IterTools: subsets
 
-end # module
+
+include("NTRsurvPrior.jl")
+include("CoxNTRsurvPrior.jl")
+include("AdaptiveMH.jl")
+include("utils.jl")
+
+export
+    BaselineNTR,
+    BaselineRegreNTR,
+    DataNTR,
+    DataRegreNTR,
+    ExponentialBaseline,
+    MCMCchainAcc,
+    ModelNTR,
+    ModelRegreNTR,
+    RobMonMHwithinGIBBStune,
+    RandWalkMHwithinGibbs,
+    SuffStatsRegreNTR,
+    cred_band_mat,
+    loglikNTR,
+    loglikRegreNTR,
+    posterior_sim,
+    postmeansurv,
+    prior_sim
+end
