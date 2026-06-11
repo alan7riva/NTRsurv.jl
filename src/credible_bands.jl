e82b2658aceb96aeed82bfcb6a4d9d5566795301
Alan <alan@sigma.iimas.unam.mx>
2026-06-11 08:25:40 -0600

    Updated credible band code so middle outputs are point estimates.

--- src/credible_bands.jl
+++ src/credible_bands.jl
@@ -3,24 +3,25 @@ function credible_band( p::Float64, S::Matrix{Float64}, μ::Bool=true)
         @error "ERROR: p is not between zero and one."
     end 
     l,k = size(S)
+    band_m = zeros(k)
+    if μ
+        band_m = vec(mean(S,dims=1))
+    else
+        band_m = vec(median(S,dims=1))
+    end
     m = round( Int, l*p)
+    S_band = copy(S)
     for _ in 1:m
         b = zeros(Float64, k)
         for i in 1:k
-            b[i] = maximum(S[:,i])-minimum(S[:,i])
+            b[i] = maximum(S_band[:,i])-minimum(S_band[:,i])
         end
         b_max_index = sortperm(b)[end]
-        s_out_index = findmax( abs.( S[:,b_max_index] .- median(S[:,b_max_index]) ) )[2]
-        S = S[1:end .!= s_out_index, 1:end  ]
+        s_out_index = findmax( abs.( S_band[:,b_max_index] .- median(S_band[:,b_max_index]) ) )[2]
+        S_band = S_band[1:end .!= s_out_index, 1:end  ]
     end
-    band_u = [ maximum(S[:,i]) for i in 1:k]
-    band_d = [ minimum(S[:,i]) for i in 1:k]
-    band_m = zeros(k)
-    if μ
-        band_m = mean(S,dims=1)[1:end]
-    else
-        band_m = median(S,dims=1)[1:end]
-    end
+    band_u = [ maximum(S_band[:,i]) for i in 1:k]
+    band_d = [ minimum(S_band[:,i]) for i in 1:k]
     return band_d, band_m, band_u
 end
 
